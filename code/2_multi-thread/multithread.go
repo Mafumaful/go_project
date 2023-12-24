bcdd3141ee818937f063d2e3d4bf56a54a630db0
mafumaful <s6bian@uwaterloo.ca>
2023-12-23 23:27:34 -0500

    Remove unused code files

---
+++ code/2_multi-thread/multithread.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func someTask(data int) {
+	time.Sleep(2 * time.Millisecond)
+	fmt.Printf("Task %d executed\n", data)
+}
+
+func main() {
+	for i := 0; i < 100; i++ {
+		someTask(i)
+	}
+}
---
+++ code/init.sh
@@ -0,0 +1,3 @@
+cd /home/docker/code
+go work init 1_hello-world
+go work init 2_multi-thread
