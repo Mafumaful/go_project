67bf2a01d9740617616e810ea0b08f5c9a04c2f6
mafumaful <s6bian@uwaterloo.ca>
2023-12-25 21:05:29 -0500

    Remove unused code

--- code/6_import/lib1/lib1.go
+++
@@ -1,9 +0,0 @@
-package lib1
-
-func Lib1Test() {
-	println("lib1.Lib2Test")
-}
-
-func init() {
-	println("lib1.init")
-}
--- code/6_import/lib2/lib2.go
+++
@@ -1,9 +0,0 @@
-package lib2
-
-func Lib2Test() {
-	println("lib2.Lib2Test")
-}
-
-func init() {
-	println("lib2.init")
-}
